826878e5a094a12a97ef596850a0c13a0806abe6
Debaditya Chakravorty <debaditya.chakravorty@gmail.com>
2017-01-08 17:59:02 -0500

    LinkedList : ReverseAddALL
    LinkedList : ReverseAddALL

--- ReverseAddALL.java
+++ ReverseAddALL.java
@@ -1,6 +1,25 @@
 package javatutorials;
 
+/*
+*You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+*Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+*Output: 7 -> 0 -> 8
+*/
+package javatutorials;
+
+/*
+*You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+*Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+*Output: 7 -> 0 -> 8
+*/
 
 import java.util.*;
 
@@ -11,12 +30,12 @@ public class ReverseAddLL {
 		List l1 = new LinkedList();
 		List l2 = new LinkedList();
 		List l3 = new LinkedList();
-		l1.add(1);
-		l1.add(5);
+		l1.add(2);
 		l1.add(4);
-		l2.add(7);
-		l2.add(8);
-		l2.add(9);
+		l1.add(3);
+		l2.add(5);
+		l2.add(6);
+		l2.add(4);
 		System.out.println(l1);
 		System.out.println(l2);
 		int rem=0,sum=0,quo=0;
@@ -25,11 +44,11 @@ public class ReverseAddLL {
 			int val1 = (int) l1.get(i);
 			int val2 = (int) l2.get(i);
 			int org_sum = val1 + val2;	
-			if( org_sum > 10)
+			if( org_sum >= 10)
 			{
 				rem=org_sum%10;
 				quo=org_sum/10;
-				if(i!=l1.size()-1)
+				if(i!=l1.size())
 				{
 					sum=rem;
 				}
@@ -37,17 +56,17 @@ public class ReverseAddLL {
 				{
 					sum=quo+org_sum;
 				}
-				//System.out.println(i+">>"+l1.size());
-				//System.out.print(rem+"---"+quo+"----"+org_sum+"----"+sum+"\n");
+				System.out.println(i+">>"+l1.size());
+				System.out.print(rem+"---"+quo+"----"+org_sum+"----"+sum+"\n");
 			}
 			else
 			{
 				System.out.print(rem+"---"+quo+"----"+org_sum+"----"+sum+"\n");
-				sum=org_sum;
+				sum=org_sum+quo;
 			}
 			System.out.print(val1+"+"+val2+"--"+sum+"\n");
 			l3.add(sum);
-			//sum=0;
+			
 		}
 		System.out.println(l3);
 		
